40d7dd32b4e2aaa9c19820a4fbbbdb83db2ea370
Nazarii-4chain <150656925+Nazarii-4chain@users.noreply.github.com>
2024-09-09 09:28:38 +0300

    feat(SPV-1028): Fixed key type (#1023)

--- src/utils/createClient.ts
+++ src/utils/createClient.ts
@@ -1,8 +1,8 @@
-import { Role, SpvWalletClientExtended } from '@/contexts';
+import { LoginType, Role, SpvWalletClientExtended } from '@/contexts';
 import { SpvWalletClient } from '@bsv/spv-wallet-js-client';
 
-export const createClient = async (role: Role, key: string, serverUrl: string) => {
-  const client = newSPVWalletClient(role, key, serverUrl);
+export const createClient = async (role: Role, key: string, serverUrl: string, type?: LoginType) => {
+  const client = newSPVWalletClient(role, key, serverUrl, type);
 
   //checks connection (serverURL) and authentication (key)
   //throws exception on failure
@@ -21,16 +21,21 @@ export const createClient = async (role: Role, key: string, serverUrl: string) =
   throw new Error('Invalid role');
 };
 
-function newSPVWalletClient(role: Role, key: string, serverUrl: string): SpvWalletClientExtended {
+function newSPVWalletClient(role: Role, key: string, serverUrl: string, type?: LoginType): SpvWalletClientExtended {
   if (role === Role.Admin) {
     return new SpvWalletClient(serverUrl, { adminKey: key }, { level: 'disabled' });
   }
 
   if (role === Role.User) {
-    if (key.startsWith('xprv')) {
+    if (type === LoginType.AccessKey) {
+      return new SpvWalletClient(serverUrl, { accessKey: key }, { level: 'disabled' });
+    }
+
+    if (type === LoginType.Xprv) {
       return new SpvWalletClient(serverUrl, { xPriv: key }, { level: 'disabled' });
     }
-    return new SpvWalletClient(serverUrl, { accessKey: key }, { level: 'disabled' });
+
+    return new SpvWalletClient(serverUrl, { xPriv: key }, { level: 'disabled' });
   }
 
   throw new Error('Invalid key format');
