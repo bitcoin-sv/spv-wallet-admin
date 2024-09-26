2466c81c98444e279b7cf4bfe9db4c8db3a14783
swierzbicki <sebastian.wierzbicki@circlekeurope.com>
2024-09-26 09:12:06 +0200

    feat(SPV-1063): setup visual test done by playwright

---
+++ playwright.config.ts
@@ -0,0 +1,78 @@
+import { defineConfig, devices } from '@playwright/test';
+import { GitHubActionOptions } from '@estruyf/github-actions-reporter';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './src/__tests__',
+  snapshotPathTemplate: '{testDir}/visual/{testName}/{arg}{ext}',
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    timeout: 5000,
+
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.01,
+    },
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : '50%',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    !process.env.CI
+      ? ['html', { outputFolder: 'playwright-report/html' }]
+      : [
+          '@estruyf/github-actions-reporter',
+          <GitHubActionOptions>{
+            title: 'Visual tests report',
+            useDetails: true,
+            showError: true,
+          },
+        ],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'yarn run dev',
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
+    timeout: 60 * 1000,
+  },
+  use: {
+    baseURL: 'http://localhost:5173',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+});
