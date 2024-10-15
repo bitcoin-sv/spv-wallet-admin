e9de108a868f90938c22b2fa82203abdedad143c
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-10-15 07:20:02 +0200

    feat(SPV-1097): disabling refresh button when isRefreshing

--- src/routes/admin/_admin.tsx
+++ src/routes/admin/_admin.tsx
@@ -159,7 +159,7 @@ function LayoutComponent() {
           <Sheet>
             <h1>SPV Wallet Admin</h1>
           </Sheet>
-          <Button variant="ghost" className="ml-auto" onClick={onRefreshClick}>
+          <Button variant="ghost" className="ml-auto" onClick={onRefreshClick} disabled={isRefreshing}>
             <RefreshCw className={cn('h-4 w-4 mr-2', isRefreshing && 'animate-spin')} />
             Refresh
           </Button>
