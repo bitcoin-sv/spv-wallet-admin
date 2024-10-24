1f9a3a13a9c612c291ed446142194dabb9410fb5
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-10-24 13:24:44 +0200

    fix(SPV-1136): handle error on parsing error content.

--- src/components/WebhookErrorComponent/WebhookErrorComponent.tsx
+++ src/components/WebhookErrorComponent/WebhookErrorComponent.tsx
@@ -1,24 +1,26 @@
-import { ErrorResponse } from '@bsv/spv-wallet-js-client';
-import { ErrorComponentProps } from '@tanstack/react-router';
-import { CustomErrorComponent } from '@/components';
-import { Ban } from 'lucide-react';
+import {ErrorResponse} from '@bsv/spv-wallet-js-client';
+import {ErrorComponentProps} from '@tanstack/react-router';
+import {CustomErrorComponent} from '@/components';
+import {Ban} from 'lucide-react';
 
 const WebhooksDisabled = () => (
   <div className="flex flex-col w-full h-[80vh] justify-center items-center">
     <p>
-      <Ban className="size-44 text-gray-500" />
+      <Ban className="size-44 text-gray-500"/>
     </p>
     <p className="mt-4 text-gray-500">Notification feature is not enabled</p>
   </div>
 );
 
-export const WebhookErrorComponent = ({ error }: ErrorComponentProps) => {
+export const WebhookErrorComponent = ({error}: ErrorComponentProps) => {
+  let errorCode = "";
   if (error instanceof ErrorResponse) {
-    const content = JSON.parse(error.content);
-    if (content.code === 'error-notifications-disabled') {
-      return <WebhooksDisabled />;
+    try {
+      errorCode = JSON.parse(error.content).code;
+    } catch (error) {
+      console.log("Got unexpected error when parsing the content of the error response", error);
     }
   }
 
-  return <CustomErrorComponent error={error} />;
+  return errorCode === 'error-notifications-disabled' ? <WebhooksDisabled/> : <CustomErrorComponent error={error}/>;
 };
