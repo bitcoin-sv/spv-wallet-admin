ca8cc8e578de8be96aec90f339f3dc1af4104e05
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-10-29 13:28:19 +0100

    fix(SPV-1136): webhooks view is hanging when notifications are disabled. (#1048)

--- src/components/CustomErrorComponent/CustomErrorComponent.tsx
+++ src/components/CustomErrorComponent/CustomErrorComponent.tsx
@@ -5,19 +5,49 @@ export interface CustomErrorComponentProps {
   error: ErrorResponse | Error;
 }
 
+interface ApiErrorInfo {
+  status: number;
+  statusText: string;
+  message: string;
+}
+
+const extractApiError = (error: ErrorResponse | Error): ApiErrorInfo | null => {
+  if (error instanceof ErrorResponse) {
+    let errorMessage = '';
+    try {
+      errorMessage = JSON.parse(error.content).message;
+    } catch (error) {
+      console.log('Got unexpected error when parsing the content of the error response', error);
+    }
+
+    return {
+      status: error.response.status,
+      statusText: error.response.statusText,
+      message: errorMessage,
+    };
+  }
+  return null;
+};
+
+const ApiError = ({ apiError }: { apiError: ApiErrorInfo }) => (
+  <>
+    <p className={'first-letter:capitalize'}> {apiError.message}</p>
+    <p>
+      {apiError.status} - {apiError.statusText}
+    </p>
+  </>
+);
+
+const ErrorMessage = ({ error }: { error: Error }) => <p>{error.message}</p>;
+
 export const CustomErrorComponent = ({ error }: CustomErrorComponentProps) => {
-  const isErrorResponse = (error: ErrorResponse | Error): error is ErrorResponse => error?.message === undefined;
+  const apiError = extractApiError(error);
 
   return (
     <div className="flex flex-col w-full h-[80vh] justify-center items-center">
       <img src={fallingMan} alt="error image" />
       <p className="mt-4">Something went wrong</p>
-      <p>{isErrorResponse(error) ? JSON.parse(error.content).message : error.message}</p>
-      {isErrorResponse(error) && (
-        <p>
-          {error.response.status} - {error.response.statusText}
-        </p>
-      )}
+      {apiError ? <ApiError apiError={apiError} /> : <ErrorMessage error={error} />}
     </div>
   );
 };
