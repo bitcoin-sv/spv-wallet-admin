7b37ac08f0141283277b2a4bf1f54542b1ef0ce7
Damian-4chain <damian.zoltowski@4chain.studio>
2025-01-15 15:51:03 +0100

    feat(SPV-1377): add contactID constant and fix search by contact id

--- src/utils/getContactId.ts
+++ src/utils/getContactId.ts
@@ -1,1 +1,3 @@
-export const getContactId = (filter: string) => (filter.length === 32 ? filter : undefined);
+import { CONTACT_ID_LENGTH } from '@/constants';
+
+export const getContactId = (filter: string) => (filter.length === CONTACT_ID_LENGTH ? filter : undefined);
